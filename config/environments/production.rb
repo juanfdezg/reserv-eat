51e86743ac71526dc7229b71decadaeaa22026fe
juanfdezg <juanfdezg@LAPTOP-0ICAJ7V6.localdomain>
2023-02-14 14:33:41 -0300

    Deploy funcionando, intento de seed para admin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.delivery_method = :smtp
-  host = 'https://reserv-eat.herokuapp.com/'
+  host = 'https://reserveat.onrender.com/'
   config.action_mailer.default_url_options = { host: host }
 
 # SMTP settings for gmail
